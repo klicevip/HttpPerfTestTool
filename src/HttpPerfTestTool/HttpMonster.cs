caf5199457c5b5effef8bec89b823d663880b59f
xieshengxin <xieshengxin@mallcoo.cn>
2016-04-18 19:05:23 +0800

    添加代码

---
+++ src/HttpPerfTestTool/HttpMonster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpPerfTestTool
+{
+    public class HttpMonster
+    {
+        public ILog Log { get; set; }
+
+        public HttpMonster(ILog log)
+        {
+            Log = log;
+        }
+
+        public string Start(string url, string method, string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop(string testId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
